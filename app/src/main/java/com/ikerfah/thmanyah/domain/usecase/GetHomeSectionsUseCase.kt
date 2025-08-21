0c5b3b5fafd42b8ebf0d486cb5ded4fc106d4e48
Issam KERFAH <i.kerfah@gmail.com>
2025-08-22 01:12:14 +0300

    fix: sort sections by order

--- app/src/main/java/com/ikerfah/thmanyah/domain/usecase/GetHomeSectionsUseCase.kt
+++ app/src/main/java/com/ikerfah/thmanyah/domain/usecase/GetHomeSectionsUseCase.kt
@@ -6,4 +6,5 @@ import com.ikerfah.thmanyah.domain.repository.AppRepository
 class GetHomeSectionsUseCase(private val repository: AppRepository) {
     suspend operator fun invoke(page: Int? = null): List<Section> =
         repository.getHomeSections(page = page)
+            .sortedBy { it.order }
 }
